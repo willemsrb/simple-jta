21ce779da978edd77015dad7d3a2af1fb46fe5b7
Robert Willems of Brilman <robert.willems.of.brilman@gmail.com>
2017-10-24 13:51:17 +0200

    Initial code

---
+++ src/test/java/nl/futureedge/simple/jta/spring/hibernate/SpringJtaPlatformTest.java
@@ -0,0 +1,139 @@
+package nl.futureedge.simple.jta.spring.hibernate;
+
+import javax.transaction.RollbackException;
+import javax.transaction.Status;
+import javax.transaction.Synchronization;
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
+import org.hibernate.engine.transaction.jta.platform.spi.JtaPlatformException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+public class SpringJtaPlatformTest {
+
+    private JtaTransactionManager springJtaTransactionManager;
+    private SpringJtaPlatform subject;
+
+    @Before
+    public void setup() {
+        springJtaTransactionManager = Mockito.mock(JtaTransactionManager.class);
+
+        subject = new SpringJtaPlatform();
+        subject.setSpringJtaTransactionManager(springJtaTransactionManager);
+    }
+
+    @Test
+    public void retrieveTransactionManager() {
+        TransactionManager result = Mockito.mock(TransactionManager.class);
+        Mockito.when(springJtaTransactionManager.getTransactionManager()).thenReturn(result);
+        Assert.assertSame(result, subject.retrieveTransactionManager());
+        Mockito.verify(springJtaTransactionManager).getTransactionManager();
+        Mockito.verifyNoMoreInteractions(springJtaTransactionManager);
+    }
+
+    @Test
+    public void retrieveUserTransaction() {
+        UserTransaction result = Mockito.mock(UserTransaction.class);
+        Mockito.when(springJtaTransactionManager.getUserTransaction()).thenReturn(result);
+        Assert.assertSame(result, subject.retrieveUserTransaction());
+        Mockito.verify(springJtaTransactionManager).getUserTransaction();
+        Mockito.verifyNoMoreInteractions(springJtaTransactionManager);
+    }
+
+    @Test
+    public void getTransactionIdentifier() {
+        Transaction transaction = Mockito.mock(Transaction.class);
+        Assert.assertSame(transaction, subject.getTransactionIdentifier(transaction));
+    }
+
+    @Test
+    public void getCurrentStatus() throws Exception {
+        TransactionManager transactionManager = Mockito.mock(TransactionManager.class);
+        Mockito.when(springJtaTransactionManager.getTransactionManager()).thenReturn(transactionManager);
+        Mockito.when(transactionManager.getStatus()).thenReturn(Status.STATUS_PREPARING);
+
+        Assert.assertEquals(Status.STATUS_PREPARING, subject.getCurrentStatus());
+
+        Mockito.verify(springJtaTransactionManager).getTransactionManager();
+        Mockito.verify(transactionManager).getStatus();
+        Mockito.verifyNoMoreInteractions(springJtaTransactionManager, transactionManager);
+    }
+
+    @Test
+    public void canRegisterSynchronization() throws Exception {
+        TransactionManager transactionManager = Mockito.mock(TransactionManager.class);
+        Mockito.when(springJtaTransactionManager.getTransactionManager()).thenReturn(transactionManager);
+        Mockito.when(transactionManager.getStatus()).thenReturn(Status.STATUS_PREPARING, Status.STATUS_ACTIVE, Status.STATUS_MARKED_ROLLBACK);
+
+        Assert.assertFalse(subject.canRegisterSynchronization());
+        Assert.assertTrue(subject.canRegisterSynchronization());
+        Assert.assertFalse(subject.canRegisterSynchronization());
+
+        Mockito.verify(springJtaTransactionManager, Mockito.times(3)).getTransactionManager();
+        Mockito.verify(transactionManager, Mockito.times(3)).getStatus();
+        Mockito.verifyNoMoreInteractions(springJtaTransactionManager, transactionManager);
+    }
+
+
+    @Test
+    public void canRegisterSynchronizationFailed() throws Exception {
+        TransactionManager transactionManager = Mockito.mock(TransactionManager.class);
+        Mockito.when(springJtaTransactionManager.getTransactionManager()).thenReturn(transactionManager);
+        Mockito.when(transactionManager.getStatus()).thenThrow(new SystemException("Test"));
+
+        try {
+            subject.canRegisterSynchronization();
+            Assert.fail("JtaPlatformException expected");
+        } catch (JtaPlatformException e) {
+            //Expected
+        }
+
+        Mockito.verify(springJtaTransactionManager).getTransactionManager();
+        Mockito.verify(transactionManager).getStatus();
+        Mockito.verifyNoMoreInteractions(springJtaTransactionManager, transactionManager);
+    }
+
+    @Test
+    public void registerSynchronization() throws Exception {
+        TransactionManager transactionManager = Mockito.mock(TransactionManager.class);
+        Transaction transaction = Mockito.mock(Transaction.class);
+        Mockito.when(springJtaTransactionManager.getTransactionManager()).thenReturn(transactionManager);
+        Mockito.when(transactionManager.getTransaction()).thenReturn(transaction);
+
+        Synchronization synchronization = Mockito.mock(Synchronization.class);
+        subject.registerSynchronization(synchronization);
+
+        Mockito.verify(springJtaTransactionManager).getTransactionManager();
+        Mockito.verify(transactionManager).getTransaction();
+        Mockito.verify(transaction).registerSynchronization(synchronization);
+        Mockito.verifyNoMoreInteractions(springJtaTransactionManager, transactionManager, transaction);
+    }
+
+    @Test
+    public void registerSynchronizationFailed() throws Exception {
+        TransactionManager transactionManager = Mockito.mock(TransactionManager.class);
+        Transaction transaction = Mockito.mock(Transaction.class);
+        Mockito.when(springJtaTransactionManager.getTransactionManager()).thenReturn(transactionManager);
+        Mockito.when(transactionManager.getTransaction()).thenReturn(transaction);
+
+        Synchronization synchronization = Mockito.mock(Synchronization.class);
+        Mockito.doThrow(new RollbackException("test")).when(transaction).registerSynchronization(synchronization);
+
+        try {
+            subject.registerSynchronization(synchronization);
+            Assert.fail("JtaPlatformException expected");
+        } catch (JtaPlatformException e) {
+            //Expected
+        }
+
+        Mockito.verify(springJtaTransactionManager).getTransactionManager();
+        Mockito.verify(transactionManager).getTransaction();
+        Mockito.verify(transaction).registerSynchronization(synchronization);
+        Mockito.verifyNoMoreInteractions(springJtaTransactionManager, transactionManager, transaction);
+    }
+}
