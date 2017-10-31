97cfb7209c73a0cf768b9f08ad1c74920f21c8df
Robert Willems of Brilman <robert.willems.of.brilman@gmail.com>
2017-10-31 12:21:54 +0100

    Initial code

--- run_mariadb.cmd
+++
@@ -1,1 +0,0 @@
-docker run -d -p 3306:3306 -e MYSQL_USER=simple -e MYSQL_PASSWORD=jta -e MYSQL_DATABASE=trans -e MYSQL_ROOT_PASSWORD=secret mariadb:10.3
--- run_postgresql.cmd
+++
@@ -1,1 +0,0 @@
-docker run -d -p 5432:5432 -e POSTGRES_USER=simple -e POSTGRES_PASSWORD=jta -e POSTGRES_DB=trans postgres:9.6 
